395042bca89b703bcc282db1e9a8c7e3c1c4a688
shake551 <73379887+shake551@users.noreply.github.com>
2022-05-07 16:18:26 +0900

    [add]全てのリクエストを受け入れる

--- opt/config/settings.py
+++ opt/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--kyli=k18ocq_gpj(k%dtu4g+&cb$#5&jfn=gh73_jl@9bdzoz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
